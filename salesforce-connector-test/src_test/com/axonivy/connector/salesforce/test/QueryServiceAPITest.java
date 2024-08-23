37b344e8d6513ca584a783de295fed968f1471e0
Pham Hoang Hung <84316773+phhung-axonivy@users.noreply.github.com>
2024-08-23 16:59:40 +0700

    Feature/marp 626 fix for dev fail on market monitor (#30)

--- salesforce-connector-test/src_test/com/axonivy/connector/salesforce/test/QueryServiceAPITest.java
+++ salesforce-connector-test/src_test/com/axonivy/connector/salesforce/test/QueryServiceAPITest.java
@@ -19,7 +19,7 @@ import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
 import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
 
-@IvyProcessTest
+@IvyProcessTest(enableWebServer = true)
 public class QueryServiceAPITest extends BaseTest {
 	private static final BpmProcess QUERYSERVICE_PROCESS = BpmProcess.path("QueryService");
 
