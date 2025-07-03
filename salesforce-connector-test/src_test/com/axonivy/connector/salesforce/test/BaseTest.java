72f8e996d3fd57c4c5c6898b14e121f43203b339
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-07-03 14:31:32 +0700

    MARP-1983 Update End2End UTs

--- salesforce-connector-test/src_test/com/axonivy/connector/salesforce/test/BaseTest.java
+++ salesforce-connector-test/src_test/com/axonivy/connector/salesforce/test/BaseTest.java
@@ -1,17 +1,17 @@
 package com.axonivy.connector.salesforce.test;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
 import ch.ivyteam.ivy.environment.AppFixture;
+import utils.SalesforceUtils;
 
 @IvyProcessTest(enableWebServer = true)
 public class BaseTest {
 
 	@BeforeEach
-	void beforeEach(AppFixture fixture) {
-		// Disable OAuth feature for mock rest service
-		fixture.config("RestClients.SalesforceAPI.Features", "ch.ivyteam.ivy.rest.client.mapper.JsonFeature");
-		fixture.var("salesforce-connector.auth.url", "{ivy.app.baseurl}/api/salesforceMock");
+	void beforeEach(ExtensionContext context, AppFixture fixture) {
+		SalesforceUtils.setUpConfigForContext(context.getDisplayName(), fixture);
 	}
 }
