5d2e32fc6232e308dd6daad803831ef7d7b2068a
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-07-14 15:13:02 +0700

    MARP-1983 Handle Feedbacks

--- salesforce-connector-test/src_test/com/axonivy/connector/salesforce/test/BaseTest.java
+++ salesforce-connector-test/src_test/com/axonivy/connector/salesforce/test/BaseTest.java
@@ -3,9 +3,10 @@ package com.axonivy.connector.salesforce.test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
+import com.axonivy.connector.salesforce.utils.SalesforceUtils;
+
 import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
 import ch.ivyteam.ivy.environment.AppFixture;
-import utils.SalesforceUtils;
 
 @IvyProcessTest(enableWebServer = true)
 public class BaseTest {
