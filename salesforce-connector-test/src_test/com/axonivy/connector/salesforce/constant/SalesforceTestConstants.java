f584bdadc127bdf0f99f6f73cbf32189bbe32612
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2025-07-31 14:33:05 +0700

    MARP-2597 Add E2E workflow and test environment support (#57)

--- salesforce-connector-test/src_test/com/axonivy/connector/salesforce/constant/SalesforceTestConstants.java
+++ salesforce-connector-test/src_test/com/axonivy/connector/salesforce/constant/SalesforceTestConstants.java
@@ -7,4 +7,6 @@ public class SalesforceTestConstants {
 	public static final String SUB_DOMAIN = "subdomain";
 	public static final String CLIENT_ID = "clientid";
 	public static final String CLIENT_SECRET = "clientsecret";
+	public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
+	public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }
