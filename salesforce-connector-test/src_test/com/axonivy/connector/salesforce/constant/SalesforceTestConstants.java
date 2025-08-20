9726b1eb76e1f0fdd4c81e600ed706f01d86c730
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-08-20 15:21:23 +0700

    MARP-2631 Removed unused codes

--- salesforce-connector-test/src_test/com/axonivy/connector/salesforce/constant/SalesforceTestConstants.java
+++ salesforce-connector-test/src_test/com/axonivy/connector/salesforce/constant/SalesforceTestConstants.java
@@ -1,12 +1,8 @@
 package com.axonivy.connector.salesforce.constant;
 
 public class SalesforceTestConstants {
-	public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
-	public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
-	public static final String URL = "url";
-	public static final String SUB_DOMAIN = "subdomain";
-	public static final String CLIENT_ID = "clientid";
-	public static final String CLIENT_SECRET = "clientsecret";
-	public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-	public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
+  public static final String URL = "url";
+  public static final String SUB_DOMAIN = "subdomain";
+  public static final String CLIENT_ID = "clientid";
+  public static final String CLIENT_SECRET = "clientsecret";
 }
--- salesforce-connector-test/src_test/com/axonivy/connector/salesforce/test/GetAccServiceAPITest.java
+++ salesforce-connector-test/src_test/com/axonivy/connector/salesforce/test/GetAccServiceAPITest.java
@@ -3,17 +3,13 @@ package com.axonivy.connector.salesforce.test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
-
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
-
-import com.axonivy.connector.salesforce.constant.SalesforceTestConstants;
-import com.axonivy.connector.salesforce.context.MultiEnvironmentContextProvider;
 import com.axonivy.connector.salesforce.model.Account;
+import com.axonivy.utils.e2etest.context.MultiEnvironmentContextProvider;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
-
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
@@ -23,20 +19,19 @@ import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
 @IvyProcessTest(enableWebServer = true)
 @ExtendWith(MultiEnvironmentContextProvider.class)
 public class GetAccServiceAPITest extends BaseTest {
-	private static final BpmProcess GETACCSERVICE_PROCESS = BpmProcess.path("GetAccService");
+  private static final BpmProcess GETACCSERVICE_PROCESS = BpmProcess.path("GetAccService");
 
-	@TestTemplate
-	void getAccount(ExtensionContext context, BpmClient bpmClient)
-			throws NoSuchFieldException, StreamReadException, DatabindException, IOException {
-		boolean isRealContext = context.getDisplayName().equals(SalesforceTestConstants.REAL_CALL_CONTEXT_DISPLAY_NAME);
-		BpmElement startable = GETACCSERVICE_PROCESS.elementName("call(String)");
+  @TestTemplate
+  void getAccount(ExtensionContext context, BpmClient bpmClient)
+      throws NoSuchFieldException, StreamReadException, DatabindException, IOException {
+    BpmElement startable = GETACCSERVICE_PROCESS.elementName("call(String)");
 
-		ExecutionResult result = bpmClient.start().subProcess(startable).execute("0015g00001UvqSvAAJ");
-		Account response = (Account) result.data().last().get("acc");
-		if (isRealContext) {
-			assertThat(response.getName()).isEqualTo("Pyramid Construction Inc.");
-		} else {
-			assertThat(response.getName()).isEqualTo("Acc test1");
-		}
-	}
+    ExecutionResult result = bpmClient.start().subProcess(startable).execute("0015g00001UvqSvAAJ");
+    Account response = (Account) result.data().last().get("acc");
+    if (isRealContext) {
+      assertThat(response.getName()).isEqualTo("Pyramid Construction Inc.");
+    } else {
+      assertThat(response.getName()).isEqualTo("Acc test1");
+    }
+  }
 }
